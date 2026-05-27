f5e77c1a1150c2d5ae125d9ebfbe2b5ddd788455
Mvandy11 <m_vandeventer@yahoo.com>
2026-05-27 19:29:29 -0400

    Delete src/modules/memberships/index.ts

--- src/modules/memberships/index.ts
+++
@@ -1,6 +0,0 @@
-export { getMembership } from './getMembership';
-export { requireMembership } from './requireMembership';
-export { useMembership } from './useMembership';
-export type { UseMembershipResult } from './useMembership';
-export type { MembershipGateOptions } from './requireMembership';
-export type { ProfileSubscription } from '../../lib/types';
