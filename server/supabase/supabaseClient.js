1ef2f93a4775b6eb6f8d4981c6d7fec89177cd29
Mvandy11 <m_vandeventer@yahoo.com>
2026-07-01 06:52:34 -0400

    Refactor Supabase client initialization
    Updated Supabase client to use service role key.

--- server/supabase/supabaseClient.js
+++ server/supabase/supabaseClient.js
@@ -1,8 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl  = process.env.SUPABASE_URL || process.env.VITE_SUPABASE_URL;
-const supabaseAnon = process.env.SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseAnon);
-
-export default supabase;
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
